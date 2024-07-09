398b633811d222573e09f931e020619334919cb5
selveresta <artem.lypnyk@softsculptor.net>
2024-07-09 22:54:48 +0300

    3 groups

--- reposterBot.py
+++ reposterBot.py
@@ -14,7 +14,11 @@ logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = ""
 GROUP_ID = -1001792317500
+GROUP_ID_TWO = -1001813741487
+GROUP_ID_THREE = -1001820466879
 MESSAGE_ID = 0
+MESSAGE_ID_TWO = 0
+MESSAGE_ID_THREE = 0
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
@@ -33,9 +37,19 @@ Join the HamsterCombat airdrop and win your share of $500 in HMSTR tokens!
 """
 
 
+text1 = """
+❗️Announced AIRDROP Hamster Kombat ❗️ 
+
+🎉 Join the Hamster Kombat airdrop and win your share of $500 in HMSTR tokens!  🎉
+This airdrop is FCFS (First Come, First Served) for the first 50,000 participants.  🚀
+
+#HamsterCombat #Airdrop #HMSTR #CryptoGiveaway #Milestone #ThankYou #OnwardAndUpward #crypto #btc #memetoken #memecoin #hamster #taptap #clicker #game #p2e
+"""
+
+
 @router.message(Command("start"))
 async def send_new_post(message: types.Message, bot: Bot):
-    global MESSAGE_ID, GROUP_ID
+    global MESSAGE_ID, GROUP_ID, GROUP_ID_TWO, MESSAGE_ID_TWO, GROUP_ID_THREE, MESSAGE_ID_THREE
 
     min10inSec = 60 * 10
 
@@ -46,25 +60,52 @@ async def send_new_post(message: types.Message, bot: Bot):
             url="https://hamsters-kombats.site/",
         )
     )
+    keyboard = keyboard_builder.as_markup()
+
+    keyboard_builder1 = InlineKeyboardBuilder()
+    keyboard_builder1.add(
+        InlineKeyboardButton(
+            text="CLAIM",
+            url="https://hamster-combats.site",
+        )
+    )
+    keyboard1 = keyboard_builder1.as_markup()
 
     photo = FSInputFile("image.jpg")
 
-    keyboard = keyboard_builder.as_markup()
-
-    if MESSAGE_ID == 0:
+    if MESSAGE_ID == 0 and MESSAGE_ID_TWO == 0:
         message = await bot.send_photo(
             GROUP_ID,
             photo=photo,
             caption=text,
             reply_markup=keyboard,
         )
+
+        message2 = await bot.send_photo(
+            GROUP_ID_TWO,
+            photo=photo,
+            caption=text1,
+            reply_markup=keyboard1,
+        )
+
+        message3 = await bot.send_photo(
+            GROUP_ID_THREE,
+            photo=photo,
+            caption=text1,
+            reply_markup=keyboard1,
+        )
+
         MESSAGE_ID = message.message_id
+        MESSAGE_ID_TWO = message2.message_id
+        MESSAGE_ID_THREE = message3.message_id
 
         await asyncio.sleep(min10inSec)
         await send_new_post(message, bot)
         return
 
     await bot.delete_message(chat_id=GROUP_ID, message_id=MESSAGE_ID)
+    await bot.delete_message(chat_id=GROUP_ID_TWO, message_id=MESSAGE_ID_TWO)
+    await bot.delete_message(chat_id=GROUP_ID_THREE, message_id=MESSAGE_ID_THREE)
 
     message = await bot.send_photo(
         GROUP_ID,
@@ -73,7 +114,23 @@ async def send_new_post(message: types.Message, bot: Bot):
         reply_markup=keyboard,
     )
 
+    message2 = await bot.send_photo(
+        GROUP_ID_TWO,
+        photo=photo,
+        caption=text1,
+        reply_markup=keyboard,
+    )
+
+    message3 = await bot.send_photo(
+        GROUP_ID_THREE,
+        photo=photo,
+        caption=text1,
+        reply_markup=keyboard1,
+    )
+
     MESSAGE_ID = message.message_id
+    MESSAGE_ID_TWO = message2.message_id
+    MESSAGE_ID_THREE = message3.message_id
 
     await asyncio.sleep(min10inSec)
     await send_new_post(message, bot)
